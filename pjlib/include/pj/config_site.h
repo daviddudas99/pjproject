f6da657eec0a347400bb9ae1796cc8d2d0e34e44
Šimon Smejkal <simon.smejkal@seznam.cz>
2021-06-16 11:16:05 +0200

    modify config_site.h

--- pjlib/include/pj/config_site.h
+++ pjlib/include/pj/config_site.h
@@ -1,7 +1,11 @@
-#define PJSIP_MAX_TRANSPORTS        10
-#define PJSIP_MAX_RESOLVED_ADDRESSES    40
+#define PJSIP_MAX_TRANSPORTS        400
+#define PJSIP_MAX_RESOLVED_ADDRESSES    400
 
-#define PJSUA_MAX_ACC           10
-#define PJSUA_MAX_CALLS     16
-#define PJSUA_MAX_PLAYERS       10
-#define PJSUA_MAX_RECORDERS       10
+#define PJSUA_MAX_ACC           400
+#define PJSUA_MAX_CALLS     400
+#define PJSUA_MAX_PLAYERS       400
+#define PJSUA_MAX_RECORDERS       400
+
+#define PJ_IOQUEUE_MAX_HANDLES      1024
+#define FD_SETSIZE_SETABLE      1
+#define __FD_SETSIZE            1024
